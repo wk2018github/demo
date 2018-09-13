6d41ea48325fbf673ad4fe3e7e91d65c14dcfac6
wkang92 <807283018@qq.com>
2018-09-13 18:16:15 +0800

    Hello Word

--- InterlliJ IDEA/demo/demo/src/main/java/com/example/demo/HelloWord.java
+++ InterlliJ IDEA/demo/demo/src/main/java/com/example/demo/HelloWord.java
@@ -1,4 +1,20 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWord {
+
+    @RequestMapping(value = "/hello")
+    public String index(){
+        return  "hello word!";
+    }
+
+    @RequestMapping(value = "/index/getName")
+    public String getName(String name,String password){
+        return "世界卫生组织"+name+password;
+    }
 }
+
+
